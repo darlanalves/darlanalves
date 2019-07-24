85d67f11026d240749f6b16cf74ce14b1063e598
Darlan Alves <darlan.alves@tnt-digital.com>
2019-07-24 13:39:27 +0200

    chore: add build script

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,10 @@
-#!/bin/bash
-
-# Example of .env file
-# export BUILD_DIR="/opt/deploy/tmp"
-# export TARGET_DIR="/opt/www/blog"
-# export BLOG_REPO="https://github.com/darlanalves/darlanalves.git"
-
-source .env
-set -e
-
-echo 'Starting deploy... '
-
-if [ -d "$BUILD_DIR" ]; then
-    cd "$BUILD_DIR"
-    git reset --hard HEAD
-    git checkout master
-    git fetch
-    git rebase
-else
-    git clone -b master $BLOG_REPO $BUILD_DIR
-fi
-
-if [ -d "$BUILD_DIR" ]; then
-    cd $BUILD_DIR
-    npm update && hexo clean && hexo generate;
-    rm -rf $TARGET_DIR &&
-    mv $BUILD_DIR/public $TARGET_DIR
-    echo OK
-else
-    echo 'Failed!';
-    exit 1;
-fi
+hexo clean
+hexo generate
+cp CNAME public/
+cd public
+git init
+git remote add origin git@github.com:darlanalves/darlanalves.github.io.git
+git add .
+git commit -m "chore: update site content"
+git push origin master -f
+cd -
